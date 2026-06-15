6206e491bb7148a2fa67d03a586fdfef904abd99
Adamzou-lab <adamzpro@icloud.com>
2026-06-15 18:24:06 +0200

    refactor: remove type hint from async_generator function for simplicity

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,9 @@
 """Module providing an asynchronous generator."""
 import asyncio
 import random
-from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator():
     """Yield a random float between 0 and 10, ten times, with 1s delay each."""
     for _ in range(10):
         await asyncio.sleep(1)
